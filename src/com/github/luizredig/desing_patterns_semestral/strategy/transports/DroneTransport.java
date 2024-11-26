29d84f3600762848459d619ee2781db4d2cfe0d5
redig <luizredig@gmail.com>
2024-11-26 19:53:43 -0300

    feat(transport): add DroneTransport strategy implementation

---
+++ src/com/github/luizredig/desing_patterns_semestral/strategy/transports/DroneTransport.java
@@ -0,0 +1,14 @@
+package com.github.luizredig.desing_patterns_semestral.strategy.transports;
+
+import com.github.luizredig.desing_patterns_semestral.strategy.Package;
+import com.github.luizredig.desing_patterns_semestral.strategy.interfaces.TransportStrategy;
+
+public class DroneTransport implements TransportStrategy {
+    @Override
+    public String calculateDelivery(Package pkg) {
+        if (!pkg.getLocation().equalsIgnoreCase("metropolitan") || pkg.getWeight() > 5) {
+            return "Entrega por drones está disponível apenas para áreas metropolitanas e pacotes pequenos.";
+        }
+        return "Usando entrega por drones.";
+    }
+}
